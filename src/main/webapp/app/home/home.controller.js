377233ffa87e3ce8ec2b3dec53eeb23fbd56a809
Ursin Brunner <ursin.brunner@gmail.com>
2016-09-29 08:27:31 +0200

    load logged-in user resource after successful login

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,32 +1,50 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('awesomoApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'User'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController($scope, Principal, LoginService, $state, User) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
+
+        vm.newWorkLog = function () {
+            return {
+                approved: false,
+                workFrom: null,
+                workTo: null,
+                id: null,
+                user: null,
+                project: null
+            }
+        };
+
+        $scope.$on('authenticationSuccess', function () {
             getAccount();
         });
 
         getAccount();
 
         function getAccount() {
-            Principal.identity().then(function(account) {
+            Principal.identity().then(function (account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
+
+                User.get({login:vm.account.login},function (user) {
+                    vm.newWorkLog.user = user;
+                });
             });
         }
-        function register () {
+
+
+        function register() {
             $state.go('register');
         }
     }
